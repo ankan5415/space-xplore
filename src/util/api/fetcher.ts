ed8023d7a1a183fee4b938cf1d4ee1eff26afc69
ankan5415 <anks5415@gmail.com>
2022-01-17 20:52:44 -0500

    fixed project folder structure

--- src/util/api/fetcher.ts
+++ src/util/api/fetcher.ts
@@ -3,7 +3,7 @@ const fetcher = async (url: string, count: number) => {
   const res = await axios.get(`${process.env.NEXT_PUBLIC_API_HOST}/${url}`, {
     params: {
       api_key: process.env.NEXT_PUBLIC_API_KEY,
-      start_date: "2021-09-01",
+      start_date: "2021-12-01",
       end_date: "2022-01-17",
     },
   });
